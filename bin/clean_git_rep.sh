bb9ce28841ee5f32610ac49047d48cadeaeb16b1
Abrosimov Kirill <k.abrosimov@corp.badoo.com>
2014-12-08 11:42:55 +0300

    clean git rep in home

--- bin/clean_git_rep.sh
+++ bin/clean_git_rep.sh
@@ -4,8 +4,12 @@ git tag | xargs git tag -d; git branch -a | grep -Eo "origin/build_.*" | xargs g
 git remote prune origin
 git prune
 git gc --aggressive
-$HOME/bin/www1d3Sync badoo
-echo dsync | ssh kabrosimov@www1.d3
-echo "touch /home/kabrosimov/test_by_cron" | ssh kabrosiov@www1.d3
 popd
 echo "Cleaning completed at `date`"
+
+pushd $HOME/badoo/
+git tag | xargs git tag -d; git branch -a | grep -Eo "origin/build_.*" | xargs git branch -rd
+git remote prune origin
+git prune
+git gc --aggressive
+popd
