91abcce16153a37b9593be805fd181e3fb09ddb9
Abrosimov Kirill <k.abrosimov@corp.badoo.com>
2015-01-26 11:51:07 +0300

    fixes

--- bin/clean_git_rep.sh
+++ bin/clean_git_rep.sh
@@ -7,7 +7,7 @@ git gc --aggressive
 popd
 echo "Cleaning completed at `date`"
 
-pushd $HOME/badoo/
+pushd $HOME
 git tag | xargs git tag -d; git branch -a | grep -Eo "origin/build_.*" | xargs git branch -rd
 git remote prune origin
 git prune
